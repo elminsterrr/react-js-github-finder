5bd2df48531123eac74ce7680de4d4b61c36ab38
elminsterrr <przemyslaw.glab@gmail.com>
2018-01-02 21:13:23 +0100

    Add dependencies and config main index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeWithMiddleware = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={storeWithMiddleware}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
